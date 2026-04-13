187b17e498304b53daa2883ceaf54a0d69ac530c
DonShanilka <shanilka799@gmail.com>
2026-04-13 22:13:49 +0530

    update item routers

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -4,9 +4,9 @@ import * as controller from "../controllers/item.controller";
 const router = Router();
 
 router.post("/saveItem", controller.createItem);
-router.get("/", controller.getAllItems);
-router.get("/:id", controller.getItemById);
-router.put("/:id", controller.updateItem);
-router.delete("/:id", controller.deleteItem);
+router.get("/getAllItem", controller.getAllItems);
+router.get("/getItemById/:id", controller.getItemById);
+router.put("/updateItem/:id", controller.updateItem);
+router.delete("/deleteItem/:id", controller.deleteItem);
 
 export default router;
