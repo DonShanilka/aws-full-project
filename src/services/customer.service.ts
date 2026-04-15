04d24d131acc258570eedda7c17d972737a72349
DonShanilka <shanilka799@gmail.com>
2026-04-15 20:07:10 +0530

    add updateCustomerService to customer service layer

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -3,5 +3,9 @@ import {Customer} from "../models/customer.model";
 
 export const createCustomerService = async (data: Customer) => {
     return repo.createCustomer(data);
-}
+};
+
+export const updateCustomerService = async (id: number, data: Customer) => {
+    return repo.updateCustomer(id, data);
+};
 
